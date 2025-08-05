c83cec564c1e2c01083e7c3f94661a07b742b4ad
Kennedy Menezes <menezes_kennedy@hotmail.com>
2025-08-05 20:41:22 -0300

    livro java

---
+++ Java-ComoProgramar-Livro/C2/Comparison.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Comparison {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int n1, n2;
+
+        System.out.print("Enter firt integer: ");
+        n1 = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        n2 = input.nextInt();
+
+        if(n1 == n2)
+            System.out.printf("%d == %d%n", n1, n2);
+
+        if(n1 != n2)
+            System.out.printf("%d != %d%n", n1, n2);
+
+        if(n1 < n2)
+            System.out.printf("%d < %d%n", n1, n2);
+
+        if(n1 > n2)
+            System.out.printf("%d > %d%n", n1, n2);
+
+        if(n1 <= n2)
+            System.out.printf("%d <= %d%n", n1, n2);
+
+        if(n1 >= n2)
+            System.out.printf("%d >= %d%n", n1, n2);
+
+        input.close();
+
+    }
+
+}
