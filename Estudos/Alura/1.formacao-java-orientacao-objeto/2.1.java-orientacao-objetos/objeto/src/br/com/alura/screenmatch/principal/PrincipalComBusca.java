605baa824bb2200a4547d4c15df7946ea27dfa47
Kennedy Menezes <menezes_kennedy@hotmail.com>
2026-06-09 14:41:37 -0300

    trabalhando com api e serializacao

---
+++ Estudos/Alura/1.formacao-java-orientacao-objeto/2.1.java-orientacao-objetos/objeto/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,43 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String[] buscaSeparada = busca.split(" ");
+        String buscaTratada = String.join("-",buscaSeparada);
+//        String buscaTratada = String.join("-", busca.split(" ")); // em uma linha
+
+        String endereco = "https://omdbapi.com/?t=" + buscaTratada + "&apikey=6585022c";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
+
+
+    }
+}
