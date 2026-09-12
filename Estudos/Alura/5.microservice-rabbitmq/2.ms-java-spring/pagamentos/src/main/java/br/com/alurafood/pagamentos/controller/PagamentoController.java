39e9981da3cf53aacfabb114b706eb111e5ccb6e
Kennedy Menezes <menezes_kennedy@hotmail.com>
2026-09-12 18:43:04 -0300

    estudos microsservicos com java

--- Estudos/Alura/5.microservice-rabbitmq/2.ms-java-spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ Estudos/Alura/5.microservice-rabbitmq/2.ms-java-spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -1,4 +1,56 @@
 package br.com.alurafood.pagamentos.controller;
 
+import br.com.alurafood.pagamentos.dto.PagamentoDTO;
+import br.com.alurafood.pagamentos.service.PagamentoService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/pagamentos")
 public class PagamentoController {
+
+    @Autowired
+    PagamentoService pagamentoService;
+
+    @GetMapping
+    public Page<PagamentoDTO> listar(@PageableDefault(size=10)Pageable paginacao){
+        return pagamentoService.obterTodos(paginacao);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PagamentoDTO> detalhar(@PathVariable @NotNull Long id){
+        PagamentoDTO dto = pagamentoService.obterPorId(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PagamentoDTO> cadastrar(@RequestBody @Valid PagamentoDTO dto,
+                                                  UriComponentsBuilder uriBuilder){
+        PagamentoDTO pagamento = pagamentoService.criarPagamento(dto);
+        URI endereco = uriBuilder.path("/pagamento/{id}").buildAndExpand(pagamento.getId()).toUri();
+        return ResponseEntity.created(endereco).body(pagamento);
+    }
+
+    @PutMapping
+    public ResponseEntity<PagamentoDTO> atualizar(@PathVariable @NotNull Long id,
+                                                  @RequestBody @Valid PagamentoDTO dto){
+        PagamentoDTO atualizado = pagamentoService.atualizarPagamento(id, dto);
+        return ResponseEntity.ok(atualizado);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<PagamentoDTO> remover(@PathVariable @NotNull Long id){
+        pagamentoService.excluirPagamento(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- Estudos/Alura/5.microservice-rabbitmq/2.ms-java-spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/repository/PagamentoRepository.java
+++ Estudos/Alura/5.microservice-rabbitmq/2.ms-java-spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/repository/PagamentoRepository.java
@@ -1,4 +1,10 @@
 package br.com.alurafood.pagamentos.repository;
 
-public interface Pagamento {
+import br.com.alurafood.pagamentos.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+
+    
+
 }
