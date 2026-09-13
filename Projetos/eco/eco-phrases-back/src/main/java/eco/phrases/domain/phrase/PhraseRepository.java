03f85d9bae463215d7d9e8731508c6893e8ec596
Kennedy Menezes <menezes_kennedy@hotmail.com>
2026-09-13 15:47:25 -0300

    overdue para considerar o dia atual (estava considerando apenas antes do dia atual)

--- Projetos/eco/eco-phrases-back/src/main/java/eco/phrases/domain/phrase/PhraseRepository.java
+++ Projetos/eco/eco-phrases-back/src/main/java/eco/phrases/domain/phrase/PhraseRepository.java
@@ -27,7 +27,7 @@ public interface PhraseRepository extends JpaRepository<Phrase, Long>{
     @Query("UPDATE Phrases p SET p.nextSeen = :nextSeen WHERE p.id = :phraseId AND p.user.id = :userId")
     int updateNextSeen(Long phraseId, LocalDateTime nextSeen, Long userId);
 
-    @Query("SELECT p FROM Phrases p WHERE p.user.id = :id AND p.nextSeen < CURRENT_DATE ")
+    @Query("SELECT p FROM Phrases p WHERE p.user.id = :id AND p.nextSeen <= CURRENT_DATE ")
     List<Phrase> findOverduePhrases(Long id);
 
     List<Phrase> findByUserOrderByNextSeenAsc(User usuario);
